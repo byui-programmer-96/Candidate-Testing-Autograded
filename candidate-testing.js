203f27d05f0cdd0cba512a5623b7014066ba9f8c
byui-programmer-96 <lynnwriter96@gmail.com>
2024-07-01 21:37:22 -0500

    now it grades the user :)

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,10 +1,9 @@
 const input = require('readline-sync');
 
-
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
@@ -45,22 +44,49 @@ function askQuestion() {
 
 function gradeQuiz(candidateAnswers) {
   
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  for (let i = 0; i < candidateAnswers.length; i++) {
-    if (candidateAnswers[i] === correctAnswers[i]) {
-      console.log(`Question ${i + 1}: That is the Correct Answer! |Your Answer: ${candidateAnswers[i]}.`);
-    } else {
-      console.log(`Question ${i + 1}: Sorry, Wrong Answer! |Your Answer: ${candidateAnswers[i]} |Correct Answer: ${correctAnswers[i]}.`);
-    }
-  }
 
+ 
   let grade = 0;  //TODO 3.2 use this variable to calculate the candidates score.
   for (let i = 0; i < candidateAnswers.length; i++) {
     if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()) {
       grade += 1;
     }
   }
+  let gradeNumber = grade;
   grade = grade / questions.length * 100;
+
+  let passStatus = "";
+  if (gradeNumber >= 4) {
+    passStatus = "PASSED";
+  } else {
+    passStatus = "FAILED";
+  }
+
+  console.log(`
+    Candidate Name: ${candidateName}
+    1) Who was the first American woman in space?
+    Your Answer: ${candidateAnswers[0]}
+    Correct Answer: Sally Ride
+
+    2) True or false: 5000 meters = 5 kilometers.
+    Your Answer: ${candidateAnswers[1]}
+    Correct Answer: true
+
+    3) (5 + 3)/2 * 10 = ?
+    Your Answer: ${candidateAnswers[2]}
+    Correct Answer: 40
+
+    4) Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2?
+    Your Answer: ${candidateAnswers[3]}
+    Correct Answer: Trajectory
+
+    5) What is the minimum crew size for the ISS?
+    Your Answer: ${candidateAnswers[4]}
+    Correct Answer: 3
+
+    >>> Overall Grade: ${grade}% (${gradeNumber} of 5 responses correct) <<<
+    >>> Status: ${passStatus} <<<
+  `)
 
   return grade;
 }
@@ -68,7 +94,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log(`Hello, ${candidateName}`);
+  console.log(`Hello, ${candidateName}`);
   askQuestion();
   gradeQuiz(candidateAnswers);
 }
